9f8331975480cc37623ab5fcd23bca0de0f72d8c
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-08 19:38:52 +0300

    refactor: Update linkShortenerService to use axios params and headers
    The code changes in linkShortenerService.js update the way the API request is made to the Shareaholic API. Instead of directly concatenating the API key and URL in the URL string, the code now uses the `params` option in the axios request to pass the API key and URL as query parameters. Additionally, a `User-Agent` header is added to the request to solve the problem of returning HTML instead of JSON. This improves the readability and maintainability of the code.

--- src/services/linkShortenerService.js
+++ src/services/linkShortenerService.js
@@ -4,16 +4,24 @@ const { shareaholicAPIKey, shareaholicAPIURL } = require("../config");
 
 const shortenLink = async (url) => {
   try {
-    // problem with axios
-    // const response = await axios.get(
-    //   `https://www.shareaholic.com/v2/share/shorten_link?apikey=${shareaholicAPIKey}&url=${url}`
+    const response = await axios.get(`${shareaholicAPIURL}`, {
+      params: {
+        apikey: shareaholicAPIKey,
+        url: url,
+      },
+      headers: {
+        // used to solve the problem of returning html instead of json
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0. 0.0 Safari/537.36",
+      },
+    });
+    return response.data?.data;
+    // console.log("response :>> ", response);
+    // const response = await fetch(
+    //   `${shareaholicAPIURL}?apikey=${shareaholicAPIKey}&url=${url}`
     // );
-    // return response.data;
-    const response = await fetch(
-      `${shareaholicAPIURL}?apikey=${shareaholicAPIKey}&url=${url}`
-    );
-    const { data } = await response.json();
-    return data;
+    // const { data } = await response.json();
+    // return data;
   } catch (error) {
     throw error;
   }
