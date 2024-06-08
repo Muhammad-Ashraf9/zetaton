1b9242e02522ca2456b39c17c003d1a85377334f
Muhammad Ashraf <muhammad.ashraf.tahaa@gmail.com>
2024-06-08 13:44:54 +0300

    chore: Add shareaholicAPIURL to config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -9,6 +9,7 @@ const db = require("./firebase");
 module.exports = {
   PORT: process.env.PORT || 3000,
   shareaholicAPIKey: process.env.SHAREAHOLIC_API_KEY,
+  shareaholicAPIURL:"https://www.shareaholic.com/v2/share/shorten_link",
   db,
   IMAGES_COLLECTION: "images",
 };
--- src/services/linkShortenerService.js
+++ src/services/linkShortenerService.js
@@ -1,15 +1,16 @@
 const axios = require("axios");
 
-const { shareaholicAPIKey } = require("../config");
+const { shareaholicAPIKey, shareaholicAPIURL } = require("../config");
 
 const shortenLink = async (url) => {
   try {
+    // problem with axios
     // const response = await axios.get(
     //   `https://www.shareaholic.com/v2/share/shorten_link?apikey=${shareaholicAPIKey}&url=${url}`
     // );
     // return response.data;
     const response = await fetch(
-      `https://www.shareaholic.com/v2/share/shorten_link?apikey=${shareaholicAPIKey}&url=${url}`
+      `${shareaholicAPIURL}?apikey=${shareaholicAPIKey}&url=${url}`
     );
     const { data } = await response.json();
     return data;
